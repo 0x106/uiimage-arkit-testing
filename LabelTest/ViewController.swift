6014183f5409cce2bb790a3a1a0a7c57f77a0e80
Jordan Campbell <jordan.evan.campbell@gmail.com>
2017-11-22 10:52:22 +1300

    creating image from label

--- LabelTest/ViewController.swift
+++ LabelTest/ViewController.swift
@@ -10,24 +10,49 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+//    var labels = [Label]()
+    var labels = [ImageFromLabel]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
+        sceneView.session.delegate = self
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        var index: Int = 0
+        var x: Float = 0.0
+        var y: Float = 0.0
+        let size: Float = 0.25
+        let N = 10
+        for i in 0 ... N-1 {
+            for k in 0 ... N-1 {
+//                let display = ImageLabel()
+//                let display = Label()
+                let display = ImageFromLabel()
+                self.labels.append(display)
+            
+                index = self.labels.count - 1
+                x = Float(i) * size - (Float(N) * size * 0.5)
+                y = Float(k) * size - (Float(N) * size * 0.5)
+            
+                self.sceneView.scene.rootNode.addChildNode(self.labels[index].node)
+            
+                self.labels[index].node.position = SCNVector3Make(x, y, -2)
+            }
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -78,3 +103,75 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
     }
 }
+
+class Label {
+    var node: SCNNode
+    var display: UILabel
+    var plane: SCNPlane
+    
+    init() {
+        node = SCNNode()
+        display = UILabel(frame: CGRect(x: CGFloat(0), y: CGFloat(0), width: CGFloat(100), height: CGFloat(50)))
+        plane = SCNPlane(width: 0.2, height: 0.1)
+        
+        display.adjustsFontSizeToFitWidth = true
+        display.numberOfLines = 1
+        display.backgroundColor = UIColor.magenta
+        
+        plane.firstMaterial?.diffuse.contents = display
+        node.geometry = plane
+        
+        display.text = "test"
+    }
+}
+
+class ImageFromLabel {
+    var node: SCNNode
+    var display: UILabel
+    var plane: SCNPlane
+    var image: UIImage
+    
+    init() {
+        node = SCNNode()
+        display = UILabel(frame: CGRect(x: CGFloat(0), y: CGFloat(0), width: CGFloat(100), height: CGFloat(50)))
+        plane = SCNPlane(width: 0.2, height: 0.1)
+        
+        display.adjustsFontSizeToFitWidth = true
+        display.numberOfLines = 1
+        display.backgroundColor = UIColor.magenta
+        
+        display.text = "test"
+        image = UIImage.imageWithLabel(label: display)
+        
+        plane.firstMaterial?.diffuse.contents = image
+        node.geometry = plane
+    }
+}
+
+
+class ImageLabel {
+    var node: SCNNode
+    var display: UIImage
+    var plane: SCNPlane
+    
+    init() {
+        node = SCNNode()
+        display = UIImage(named: "Image")!
+        plane = SCNPlane(width: 0.2, height: 0.1)
+        
+        plane.firstMaterial?.diffuse.contents = display
+        node.geometry = plane
+    }
+}
+
+extension UIImage {
+    class func imageWithLabel(label: UILabel) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(label.bounds.size, false, 0.0)
+        label.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let img = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return img
+    }
+}
+
+
